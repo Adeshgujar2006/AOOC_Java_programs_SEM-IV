93964272e98943bfab0dff42fab5a3fe1af776d7
Adesh <aadeshgujar9689@gmail.com>
2026-03-10 18:37:40 +0530

    Update Matrix.java

--- Matrix.java
+++ Matrix.java
@@ -1,78 +1,87 @@
-//Java program to perform 2×2 matrix multiplication, addition, and transpose 
-//(using a switch case).
 import java.util.Scanner;
 public class Matrix {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        int[][] matrix1 = new int[2][2];
-        int[][] matrix2 = new int[2][2];
-
-        System.out.println("Enter elements of first 2x2 matrix:");
-        for (int i = 0; i < 2; i++) {
-            for (int j = 0; j < 2; j++) {
-                matrix1[i][j] = scanner.nextInt();
-            }
-        }
-
-        System.out.println("Enter elements of second 2x2 matrix:");
-        for (int i = 0; i < 2; i++) {
-            for (int j = 0; j < 2; j++) {
-                matrix2[i][j] = scanner.nextInt();
-            }
-        }
-
-        System.out.println("Choose an operation: 1. Addition 2. Multiplication 3. Transpose");
-        int choice = scanner.nextInt();
-
-        switch (choice) {
-            case 1 -> {
-                int[][] sum = addMatrices(matrix1, matrix2);
-                System.out.println("Sum of matrices:");
-                printMatrix(sum);
-            }
-            case 2 -> {
-                int[][] product = multiplyMatrices(matrix1, matrix2);
-                System.out.println("Product of matrices:");
-                printMatrix(product);
-            }
-            case 3 -> {
-                System.out.println("Transpose of first matrix:");
-                printMatrix(transposeMatrix(matrix1));
-                System.out.println("Transpose of second matrix:");
-                printMatrix(transposeMatrix(matrix2));
-            }
-            default -> System.out.println("Invalid choice");
-        }
-    }
-
-    public static int[][] addMatrices(int[][] m1, int[][] m2) {
+        int[][] matrixA = new int[2][2];
+        int[][] matrixB = new int[2][2];
         int[][] result = new int[2][2];
+
+        System.out.println("Enter elements of Matrix A (2x2):");
         for (int i = 0; i < 2; i++) {
-             for (int j = 0; j < 2; j++) {
-                result[i][j] = m1[i][j] + m2[i][j];
+            for (int j = 0; j < 2; j++) {
+                matrixA[i][j] = scanner.nextInt();
             }
         }
-        return result;
+
+        System.out.println("Enter elements of Matrix B (2x2):");
+        for (int i = 0; i < 2; i++) {
+            for (int j = 0; j < 2; j++) {
+                matrixB[i][j] = scanner.nextInt();
+            }
+        }
+
+        while (true) {
+            System.out.println("Choose an operation:");
+            System.out.println("1. Matrix Addition");
+            System.out.println("2. Matrix Multiplication");
+            System.out.println("3. Transpose of Matrix A");
+            System.out.println("4. Transpose of Matrix B");
+            System.out.println("5. Exit");
+            int choice = scanner.nextInt();
+
+            switch (choice) {
+                case 1:
+                
+                    for (int i = 0; i < 2; i++) {
+                        for (int j = 0; j < 2; j++) {
+                            result[i][j] = matrixA[i][j] + matrixB[i][j];
+                        }
+                    }
+                    System.out.println("Result of Matrix Addition:");
+                    printMatrix(result);
+                    break;
+
+                case 2:
+                    
+                    for (int i = 0; i < 2; i++) {
+                        for (int j = 0; j < 2; j++) {
+                            result[i][j] = matrixA[i][0] * matrixB[0][j] + matrixA[i][1] * matrixB[1][j];
+                        }
+                    }
+                    System.out.println("Result of Matrix Multiplication:");
+                    printMatrix(result);
+                    break;
+
+                case 3:
+                    
+                    for (int i = 0; i < 2; i++) {
+                        for (int j = 0; j < 2; j++) {
+                            result[j][i] = matrixA[i][j];
+                        }
+                    }
+                    System.out.println("Result of Transpose of Matrix A:");
+                    printMatrix(result);
+                    break;
+
+                case 4:
+                    
+                    for (int i = 0; i < 2; i++) {
+                        for (int j = 0; j < 2; j++) {
+                            result[j][i] = matrixB[i][j];
+                        }
+                    }
+                    System.out.println("Result of Transpose of Matrix B:");
+                    printMatrix(result);
+                    break;
+
+                case 5:
+                    System.out.println("Exiting...");
+                    scanner.close();
+                    return;
+            }
+        }
     }
 
-    public static int[][] multiplyMatrices(int[][] m1, int[][] m2) {
-        int[][] result = new int[2][2];
-        for (int i = 0; i < 2; i++) {
-            for (int j = 0; j < 2; j++) {
-                result[i][j] = m1[i][0] * m2[0][j] + m1[i][1] * m2[1][j];
-            }
-        }
-        return result;
-    }
-    public static int[][] transposeMatrix(int[][] m) {
-        int[][] result = new int[2][2];
-        for (int i = 0; i < 2; i++) {
-            for (int j = 0; j < 2; j++) {
-                result[j][i] = m[i][j];
-            }
-        }
-        return result;
-    }
     public static void printMatrix(int[][] matrix) {
         for (int i = 0; i < 2; i++) {
             for (int j = 0; j < 2; j++) {
@@ -82,3 +91,4 @@ public class Matrix {
         }
     }
 }
+
