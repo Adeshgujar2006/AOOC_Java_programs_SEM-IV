3afabd7fb77d0955865556b8a51e69c3916a2404
Adesh <aadeshgujar9689@gmail.com>
2026-04-07 11:21:00 +0530

    Create Bank.java

---
+++ Bank.java
@@ -0,0 +1,45 @@
+class BankAccount { 
+    protected double balance; 
+ 
+    public BankAccount(double balance) { 
+        this.balance = balance; 
+    } 
+ 
+    public void deposit(double amount) { 
+        balance += amount; 
+        System.out.println("Deposited: " + amount + " | Balance: " + balance); 
+    } 
+ 
+    public void withdraw(double amount) { 
+        if (amount > balance) { 
+            System.out.println("Insufficient funds."); 
+        } else { 
+            balance -= amount; 
+            System.out.println("Withdrawn: " + amount + " | Balance: " + balance); 
+        } 
+    } 
+} 
+ 
+class SavingsAccount extends BankAccount { 
+    public SavingsAccount(double balance) { 
+        super(balance); 
+    } 
+ 
+    @Override 
+    public void withdraw(double amount) { 
+        if (balance - amount < 100) { 
+            System.out.println("Withdrawal denied! Balance cannot fall below 100."); 
+        } else { 
+            balance -= amount; 
+            System.out.println("Withdrawn: " + amount + " | Balance: " + balance); 
+        } 
+    } 
+} 
+ 
+public class Bank { 
+    public static void main(String[] args) { 
+        SavingsAccount sa = new SavingsAccount(500); 
+        sa.deposit(200); 
+        sa.withdraw(550);  // allowed (balance would be 150) 
+        sa.withdraw(100);  // denied (balance would be 50 < 100) 
+    } 
